e51e2740d427083496579ea6c57d2d30675d3038
Gabriel Birke <gabriel.birke@gmail.com>
2016-01-24 11:13:43 +0100

    Improve security
    Only allow POST requests ( primitively, by checking $_POST, not by
    checking request method).
    
    Cleaned up imports in index.php

--- web/index.php
+++ web/index.php
@@ -1,13 +1,21 @@
 <?php
 
 use Birke\GeofencyProxy\LoggerFactory;
-use Birke\GeofencyProxy\MappingEngine;
+use Birke\GeofencyProxy\MappingFactory;
+use Birke\GeofencyProxy\ParameterCheckFactory;
 use Birke\GeofencyProxy\ParameterCheckException;
 use Birke\GeofencyProxy\RequestFactory;
 use GuzzleHttp\Client;
 use Symfony\Component\Yaml\Yaml;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+if ( empty( $_POST ) ) {
+    header( 'HTTP/1.0 405 Method Not Allowed' );
+    header( 'Content-Type: text/plain' );
+    die( "Only POST requests accepted" );
+}
+
 
 $configName = __DIR__ . '/../config.yml';
 if ( !file_exists( $configName ) ) {
@@ -19,9 +27,9 @@ $config = Yaml::parse( file_get_contents( $configName ) );
 $client = new Client();
 
 $logging = LoggerFactory::createFromConfig( $config['logging'] );
-$parameterCheckFactory = new \Birke\GeofencyProxy\ParameterCheckFactory( $logging->getRuleLogger() );
+$parameterCheckFactory = new ParameterCheckFactory( $logging->getRuleLogger() );
 
-$mappingEngine = \Birke\GeofencyProxy\MappingFactory::createMappingEngineFromConfig(
+$mappingEngine = MappingFactory::createMappingEngineFromConfig(
     $config['mappings'],
     $parameterCheckFactory,
     new RequestFactory(),
